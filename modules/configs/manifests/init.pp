59a09461db8cea77a84f3ae2368c7c6d707e24e6
Jimmie Hogklint <jimmie.hogklint@aptiv.com>
2018-10-15 17:03:13 +0200

    Ensure user dir and file structure

--- modules/configs/manifests/init.pp
+++ modules/configs/manifests/init.pp
@@ -1,7 +1,59 @@
 class configs ($username = 'hogklint') {
   file {"/home/$username/.Xdefaults":
-    ensure => link,
-    target => "/home/$username/tmp/user-files/Xdefaults",
+    ensure => 'link',
+    target => "/home/$username/repos/user-files/Xdefaults",
+    owner => "$username"
+  }
+
+  file {"/home/$username/.zshrc":
+    ensure => 'link',
+    target => "/home/$username/.zsh/zshrc",
+    owner => "$username"
+  }
+
+  file {"/home/$username/.gitconfig":
+    ensure => 'link',
+    target => "/home/$username/repos/user-files/gitconfig-benighted",
+    owner => "$username"
+  }
+# Could do "vim +PlugInstall +qall" here
+
+  file {"/home/$username/.tmux.conf":
+    ensure => 'link',
+    target => "/home/$username/repos/user-files/tmux-benighted.conf",
+    owner => "$username"
+  }
+
+  file {"/home/$username/.config":
+    ensure => 'directory',
+    owner => "$username"
+  }
+
+  file {"/home/$username/.config/awesome":
+    ensure => 'directory',
+    owner => "$username"
+  }
+
+  file {"/home/$username/.config/awesome/rc.lua":
+    ensure => 'link',
+    target => "/home/$username/repos/user-files/awesome-benighted.lua",
+    owner => "$username"
+  }
+
+  file {"/home/$username/local":
+    ensure => 'directory',
+    owner => "$username"
+  }
+
+  file {"/home/$username/local/bin":
+    ensure => 'link',
+    target => "/home/$username/repos/user-files/bin",
+    owner => "$username"
+  }
+
+  file {"/home/$username/.xinitrc":
+    ensure => 'link',
+    target => "/home/$username/repos/user-files/xinitrc-benighted",
     owner => "$username"
   }
 }
