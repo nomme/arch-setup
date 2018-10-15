9dbb664bfc6c2d9b78272cd3ebc00e0005fba2ae
Jimmie Hogklint <jimmie.hogklint@aptiv.com>
2018-10-15 17:02:36 +0200

    Update repo setup

--- modules/repo/manifests/init.pp
+++ modules/repo/manifests/init.pp
@@ -1,19 +1,37 @@
 class repo ($username = 'hogklint'){
-  vcsrepo {"/home/$username/tmp/vim-env":
+  vcsrepo {"/home/$username/.vim":
     ensure => present,
     provider => git,
     source => 'https://github.com/nomme/vim-env.git',
     user => "$username",
   }
 
-  vcsrepo {"/home/$username/tmp/user-files":
+  file {"/home/$username/.vim/autoload":
+    ensure => 'directory',
+    owner => "$username"
+  }
+
+  file {"/home/$username/.vim/autoload/plug.vim":
+    ensure => present,
+    source => 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
+    owner => "$username"
+  }
+
+  vcsrepo {"/home/$username/.zsh":
+    ensure => present,
+    provider => git,
+    source => 'https://github.com/nomme/zsh-env.git',
+    user => "$username",
+  }
+
+  vcsrepo {"/home/$username/repos/user-files":
     ensure => present,
     provider => git,
     source => 'https://github.com/nomme/user-files.git',
     user => "$username",
   }
 
-  vcsrepo {"/home/$username/tmp/arch-setup":
+  vcsrepo {"/home/$username/repos/arch-setup":
     ensure => present,
     provider => git,
     source => 'https://github.com/nomme/arch-setup.git',
