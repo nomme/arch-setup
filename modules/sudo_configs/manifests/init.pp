3c3d9132f9eceaaf3dafecf4a86aae28e73ba9e9
Jimmie Hogklint <jimmie.hogklint@aptiv.com>
2018-10-15 18:58:10 +0200

    Add udev rules

--- modules/sudo_configs/manifests/init.pp
+++ modules/sudo_configs/manifests/init.pp
@@ -1,7 +1,19 @@
 class sudo_configs ($username = 'hogklint') {
   file {"/etc/X11/xorg.conf.d/70-synaptics.conf":
     ensure => present,
-    source => "/home/$username/tmp/user-files/70-synaptics.conf",
+    source => "/home/$username/repos/user-files/etc_configs/70-synaptics.conf",
+    owner => "root",
+  }
+
+  file {"/etc/udev/rules.d/61-android.rules":
+    ensure => present,
+    source => "/home/$username/repos/user-files/etc_configs/51-android.rules",
+    owner => "root",
+  }
+
+  file {"/etc/udev/rules.d/95-monitor-changes.rules":
+    ensure => present,
+    source => "/home/$username/repos/user-files/etc_configs/95-monitor-changes.rules",
     owner => "root",
   }
 }
