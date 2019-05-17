4697fc529508adc6a979f02031ddd4ef553b8df6
Jimmie Hogklint <jimmie.hogklint@aptiv.com>
2019-05-17 23:20:13 +0200

    Add proxy environment config

--- modules/sudo_configs/manifests/init.pp
+++ modules/sudo_configs/manifests/init.pp
@@ -57,6 +57,76 @@ class sudo_configs ($username = 'hogklint',
       membership => minimum,
   }
 
-# Create $username with uid 1003 (same as build server)
-# Groups wheel, uucp(ttyUSB), wireshark
+  file_line {"Proxy http setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'http_proxy="http://127.0.0.1:3128"',
+    match => 'http_proxy',
+    replace => false,
+  }
+
+  file_line {"Proxy https setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'https_proxy="http://127.0.0.1:3128"',
+    match => 'https_proxy',
+    replace => false,
+  }
+
+  file_line {"Proxy ftp setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'ftp_proxy="http://127.0.0.1:3128"',
+    match => 'ftp_proxy',
+    replace => false,
+  }
+
+  file_line {"No proxy setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'NO_PROXY="localhost,127.0.0.1,localaddress"',
+    match => 'no_proxy',
+    replace => false,
+  }
+
+  file_line {"All proxy setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'all_proxy="http://127.0.0.1:3128"',
+    match => 'all_proxy',
+    replace => false,
+  }
+
+  file_line {"Proxy HTTP setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'HTTP_PROXY="http://127.0.0.1:3128"',
+    match => 'HTTP_PROXY',
+    replace => false,
+  }
+
+  file_line {"Proxy HTTPS setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'HTTPS_PROXY="http://127.0.0.1:3128"',
+    match => 'HTTPS_PROXY',
+    replace => false,
+  }
+
+  file_line {"Proxy FTP setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'FTP_PROXY="http://127.0.0.1:3128"',
+    match => 'FTP_PROXY',
+    replace => false,
+  }
+
+  file_line {"NO proxy setting":
+    path => '/etc/environment',
+    ensure => present,
+    line => 'NO_PROXY="localhost,127.0.0.1,localaddress"',
+    match => 'NO_PROXY',
+    replace => false,
+  }
+
 }
