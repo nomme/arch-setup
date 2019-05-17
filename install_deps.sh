cec67257e52287b53c398c215499d93a1bb90b37
Jimmie Hogklint <jimmie.hogklint@aptiv.com>
2019-05-17 21:18:34 +0200

    Add module for file_line

--- install_deps.sh
+++ install_deps.sh
@@ -6,3 +6,6 @@ modules_path="$repo_path/modules"
 [ -d $modules_path ] || mkdir -p "$modules_path"
 
 puppet module install --modulepath=$modules_path puppetlabs-vcsrepo
+
+# For file_line
+puppet module install --modulepath=$modules_path puppetlabs-stdlib
